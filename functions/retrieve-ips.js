1dd6afde65d019acc5a23207ec028fb7ba21c7e9
rguerin.pro@gmail.com <rguerin.pro@gmail.com>
2020-05-11 13:26:25 +0200

    feat(fs): add error management on source file read :sparkles:

--- functions/retrieve-ips.js
+++ functions/retrieve-ips.js
@@ -2,7 +2,14 @@ import { readFileSync } from 'fs';
 import logError from './log-error';
 
 export default (sourcePath) => {
-  let rawdata = readFileSync(sourcePath);
+  let rawdata;
+
+  try {
+    rawdata = readFileSync(sourcePath);
+  } catch (err) {
+    logError('Could not read file', { filePath: sourcePath });
+  }
+
   let sourceIps = JSON.parse(rawdata);
   const ips = sourceIps && sourceIps.map(({ ipAddress }) => ipAddress);
 
